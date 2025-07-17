1c288b7cd9f4d03b2dfbbe85a654950bb298279f
hazsauce <contreras.hazael@gmail.com>
2025-07-16 23:34:46 -0500

    Remove unused useState imports, will have to redeploy to fix Stripe link as I am having trouble setting up the account. Once the website is up, I will update Stripe from Sandbox to Live

--- src/pages/payment.tsx
+++ src/pages/payment.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React from 'react';
 import styles from './payment.module.css';
 import Header from '../components/Header';
 import { useRouter } from 'next/router';
